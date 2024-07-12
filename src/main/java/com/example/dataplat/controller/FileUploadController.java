d9bc57208d3324a436fd809d78870e9305aee84c
KinMan谨漫 <2376494700@qq.com>
2024-07-12 10:56:18 +0800

    初始化后端

--- src/main/java/com/example/dataplat/controller/FileUploadController.java
+++ src/main/java/com/example/dataplat/controller/FileUploadController.java
@@ -8,30 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 //@RequestMapping("/upload")
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController // 使用 @RestController 注解，表明这是一个 REST 控制器
 //@RequestMapping("/api") // 定义一个基础路径前缀
